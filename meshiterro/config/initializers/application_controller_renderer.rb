598510b0563b0fe2ba646edfcbaaffec5f83cba3
tatsumura <m.tatsuya6825@gmail.com>
2021-03-26 05:32:01 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cb20b0b51b8fc0350f95997a7d54731f6e419b649a12100b45c99f350cdf9b9dd48515e1eb9025b885bb05b19f3e486411beed236d09405cdf932078e5622013'
